bfe818fa3b4c18d691e371fa898339744bd8fac7
Колчин Леонид Евгеньевич <lekolchin@sberbank.ru>
2021-05-18 20:12:33 +0300

    connection to db and other improvements

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Server as SocketIoServer} from 'socket.io';
+import mysql from 'mysql';
+import dotEnv from 'dotenv';
 
-const io = require("socket.io")(server, {
+dotEnv.config();
+
+const PORT = process.env.PORT || 5000;
+const usersMap = new Map();
+const usersMapForAuth = new Map();
+
+const app = express();
+const server = http.createServer(app);
+
+const mySqlConfig = {
+	host: process.env.DB_URL,
+	port: process.env.DB_PORT,
+	user: process.env.DB_LOGIN,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	charset: 'UTF8',
+};
+
+console.log(mySqlConfig);
+
+const connection = mysql.createConnection(mySqlConfig);
+connection.connect();
+
+const io = new SocketIoServer(server, {
 	cors: {
 		origin: "*",
 		methods: [ "GET", "POST" ]
@@ -10,14 +37,56 @@ const io = require("socket.io")(server, {
 });
 
 app.use(cors());
-
-const PORT = process.env.PORT || 5000;
-
-const usersMap = new Map();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send('Running');
 });
+
+app.post('/login', (req, res) => {
+	if (!req.body) {
+		return res.status(400).send({
+			message: 'Bad request'
+		});
+	}
+
+	const { username, password } = req.body;
+
+	if (!usersMapForAuth.has(username)) {
+		return res.status(404).send({
+			message: 'Not found'
+		});
+	}
+
+	if (usersMapForAuth.get(username) !== password) {
+		return res.status(401).send({
+			message: 'Invalid Password'
+		});
+	}
+
+	return res.status(200).send({
+		username: username,
+	});
+});
+
+app.post('/register', (req, res) => {
+	console.log('REGISTRATION');
+	const { username, password } = req.body;
+
+	if (usersMapForAuth.has(username)) {
+		return res.status(409).send({
+			message: `User ${username} already exists`
+		});
+	}
+
+	usersMapForAuth.set(username, password);
+
+	console.log(usersMapForAuth);
+
+	res.status(200).send({
+		username: username,
+	});
+})
 
 io.on("connection", (socket) => {
 	socket.on('setUsername', (data) => {
